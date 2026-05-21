3d2a1297985746720d25e7db06e27ae5a68dde1b
JasnamSinghArora <149893112+JasnamSinghArora@users.noreply.github.com>
2026-05-21 09:56:32 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,6 +5,6 @@ constants = {
     "BATCHES" : 1,
     "MF" : "gender",
     "SF" : "sports",
-    "MODEL_PATH" : "/Users/jasnamsingharora/.cache/huggingface/hub/models--Qwen--Qwen2.5-0.5B-Instruct/snapshots/7ae557604adf67be50417f59c2c2f167def9a775"
+    "MODEL_PATH" : "Qwen/Qwen2.5-0.5B-Instruct"
 }
 constants["BATCH_SIZE"] = constants["DATASET_SIZE"] //  constants["BATCHES"]
